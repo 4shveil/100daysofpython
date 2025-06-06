303cfc4a0e9522fd5d89259e3bedd273715b6fe5
H-4sh <145373957+H-4sh@users.noreply.github.com>
2025-06-06 20:46:22 +0000

    Update calculator.py

--- Daily Projects/Day10/calculator.py
+++ Daily Projects/Day10/calculator.py
@@ -1,5 +1,4 @@
 from art import logo
-print(logo)
 
 def add(n1, n2):
     return n1 + n2
@@ -20,17 +19,23 @@ operations = {
     '/': divide
 }
 
-first_number = float(input("What is the first number for the calculation: "))
-while True:
-    input_operation = input("What would you like to do? ('+', '-', '*', '/'.: ")
-    if input_operation not in operations:
-        print("Invalid input. Please try again.")
-        continue
-    second_number = float(input("What is the second number for the calculation: "))
-    print(f"{first_number} {input_operation} {second_number}")
-    first_number = operations[input_operation](first_number, second_number)
-    print(f"Result: {first_number}")
-    if input("Continue? (y/n): ").lower().startswith('n'):
-        print("Thank you for choosing pypy Calculator cya!")
-        break
-    print(f"Continuing with previous result of the calculation...: {first_number}")
+def calculator():
+    print(logo)
+    first_number = float(input("What is the first number for the calculation: "))
+    while True:
+        input_operation = input("What would you like to do? ('+', '-', '*', '/'.: ")
+        if input_operation not in operations:
+            print("Invalid input. Please try again.")
+            continue
+        second_number = float(input("What is the second number for the calculation: "))
+        print(f"{first_number} {input_operation} {second_number}")
+        first_number = operations[input_operation](first_number, second_number)
+        print(f"Result: {first_number}")
+        if input("Continue? (y/n): ").lower().startswith('n'):
+            print("Thank you for choosing pypy Calculator cya!")
+            print("\n" * 100)
+            calculator()
+            break
+        print(f"Continuing with previous result of the calculation...: {first_number}")
+
+calculator()
