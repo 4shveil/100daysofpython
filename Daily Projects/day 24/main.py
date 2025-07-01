dada136572d10be87f9e0d304680c43f3c4f9b7a
Dudz <145373957+d0diz@users.noreply.github.com>
2025-07-01 10:34:24 -0700

    Delete Daily Projects/day 24/main.py extra file uploaded accidentaly

--- Daily Projects/day 24/main.py
+++
@@ -1,9 +0,0 @@
-# with open("/Users/Dell/Documents/python-stuff/day 24/my_file.txt") as file:
-#     contents = file.read()
-
-#     print(contents)
-#     file.close()
-
-
-with open("./day 24/my_file.txt", mode="w") as file:
-    file.write("New text.")
